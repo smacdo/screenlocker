73432cea4fb39bd50b11dcdb0671756b9b27c14c
Scott MacDonald <scott@smacdo.com>
2022-06-13 21:44:15 -0700

    add linux support
    Support Linux screen locking by searching for the existence of a known screen locking program, and running it successfully. 
    
    * utility func to run list of potential programs
    * bump version to 0.3 for linux support
    * static list of screen locking programs
    * refactor and add tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,46 @@ mod macos;
 #[cfg(target_os = "windows")]
 mod windows;
 
+#[cfg(target_os = "linux")]
+mod linux;
+
 use std::fmt;
 
 pub type Win32ErrorCode = u32;
 type Result<T> = std::result::Result<T, Error>;
 
 /// Error information explaining why the screen couldn't be locked.
-#[derive(Debug, Clone)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum Error {
     /// A Win32 API function reported an error code.
     Win32(Win32ErrorCode),
-    /// The current OS platform is not supported (yet) by screenlocker.
+    /// The current OS platform is not supported (yet) by screenlocker. Please
+    /// send a pull request or file an issue if you would like to add support!
     UnsupportedPlatform,
+    /// An error occurred when trying to run a caller specified executable.
+    ExeIoError {
+        /// String path of the command that was to be executed, or `None` if the
+        /// command was invalid UTF8.
+        cmd: Option<String>,
+        /// The error kind reported by `std::io::Error` when trying to run this
+        /// command.
+        kind: std::io::ErrorKind,
+        /// The error message reported by `std::io::Error` when trying to run
+        /// this command.
+        msg: String,
+    },
+    /// A user specified exe returned a non-zero exit code when executed.
+    NonZeroExit {
+        /// String path of the command that was to be executed, or `None` if the
+        /// command was invalid UTF8.
+        cmd: Option<String>,
+        /// Exit code returned by the user program after termination. This value
+        /// is `None` if the program was terminated by a signal instead of
+        /// normal termination.
+        exit_code: Option<i32>,
+    },
+    /// None of the provided screenlocking programs could be found.
+    NoExeFound,
 }
 
 impl fmt::Display for Error {
@@ -24,6 +52,38 @@ impl fmt::Display for Error {
             Error::Win32(ec) => write!(f, "GetLastResult() returned {}", ec),
             Error::UnsupportedPlatform => {
                 write!(f, "This platform is not supported - please file a bug")
+            }
+            Error::ExeIoError { cmd, kind, msg } => {
+                let cmd_name = match cmd {
+                    Some(cmd) => cmd.clone(),
+                    None => "with invalid utf8 name".to_string(),
+                };
+
+                write!(
+                    f,
+                    "io error {:?} ({}) when running user program {}",
+                    kind, msg, cmd_name
+                )
+            }
+            Error::NonZeroExit { cmd, exit_code } => {
+                let reason = match exit_code {
+                    Some(exit_code) => {
+                        format!("non-succesful exit-code {}", exit_code)
+                    }
+                    None => {
+                        format!("signal termination")
+                    }
+                };
+
+                let cmd_name = match cmd {
+                    Some(cmd) => cmd.clone(),
+                    None => "with invalid utf8 name".to_string(),
+                };
+
+                write!(f, "{} when running user program {}", reason, cmd_name)
+            }
+            Error::NoExeFound => {
+                write!(f, "none of the provided user programs were found")
             }
         }
     }
@@ -38,8 +98,9 @@ pub fn lock_screen() -> Result<()> {
     #[cfg(target_os = "windows")]
     return crate::windows::lock_screen_windows();
 
-    // TODO(scott): Add linux support.
+    #[cfg(target_os = "linux")]
+    return crate::linux::lock_screen_linux();
 
-    #[cfg(not(any(target_os = "windows", target_os = "macos")))]
+    #[cfg(not(any(target_os = "linux", target_os = "macos", target_os = "windows")))]
     return Err(Error::UnsupportedPlatform);
 }
---
+++ src/linux.rs
@@ -0,0 +1,235 @@
+use std::path::Path;
+use std::process::Command;
+
+use crate::{Error, Result};
+
+struct LockCommand<'a> {
+    exe: &'a str,
+    args: &'a [&'a str],
+}
+
+static DEFAULT_COMMANDS: [LockCommand; 6] = [
+    LockCommand {
+        exe: "/usr/bin/xdg-screensaver",
+        args: &["lock"],
+    },
+    LockCommand {
+        exe: "/usr/bin/gnome-screensaver-command",
+        args: &["--lock"],
+    },
+    LockCommand {
+        exe: "/usr/bin/qdbus",
+        args: &["org.freedesktop.ScreenSaver", "/ScreenSaver", "Lock"],
+    },
+    LockCommand {
+        exe: "/usr/bin/xscreensaver-command",
+        args: &["-lock"],
+    },
+    LockCommand {
+        exe: "/usr/bin/xlock",
+        args: &["-mode", "blank"],
+    },
+    LockCommand {
+        exe: "/usr/bin/slock",
+        args: &[],
+    },
+];
+
+pub fn lock_screen_linux() -> Result<()> {
+    // TODO(smacdo): Add user customization via config file.
+    run_first_found_exe(&DEFAULT_COMMANDS).map(|_| ())
+}
+
+/// Search through a list of programs and execute the first one that is found on
+/// the system. Return the result of running said command - either success if
+/// the commmand returned 0, or an error code indicating what went wrong.
+///
+/// Developers should accomodate users with different system setups by including
+/// multiple absolute paths to well known screen locking programs covering the
+/// majority of desktop environments (Gnome, KDE, XFCE etc). If a hardcoded list
+/// is not sufficient then consider offering a user editable configuration file
+/// for endusers to customize.
+fn run_first_found_exe(possible_cmds: &[LockCommand]) -> Result<usize> {
+    fn io_error(cmd: &Command, e: &std::io::Error) -> Error {
+        Error::ExeIoError {
+            cmd: cmd.get_program().to_str().map(|s| s.to_string()),
+            kind: e.kind(),
+            msg: e.to_string(),
+        }
+    }
+
+    // Find first command that exists on the system.
+    let cmd_pos = possible_cmds
+        .iter()
+        .position(|cmd| Path::is_file(Path::new(cmd.exe)));
+
+    // Execute the selected command, or return an error if no command could be
+    // found on the user's system.
+    match cmd_pos {
+        Some(cmd_pos) => {
+            let cmd = &possible_cmds[cmd_pos];
+
+            let mut runnable = Command::new(cmd.exe);
+            runnable.args(cmd.args.clone());
+
+            match runnable.output() {
+                Ok(output) => {
+                    // Only return sucess if the command executed succesfully and
+                    // returned an error code of zero.
+                    if output.status.success() {
+                        Ok(cmd_pos)
+                    } else {
+                        Err(Error::NonZeroExit {
+                            cmd: Some(cmd.exe.to_string()),
+                            exit_code: output.status.code(),
+                        })
+                    }
+                }
+                Err(e) => Err(io_error(&runnable, &e)),
+            }
+        }
+        None => Err(Error::NoExeFound),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::path::PathBuf;
+
+    fn path_to_test_cmd(script_name: &str) -> PathBuf {
+        let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        d.push("tests/");
+        d.push(script_name);
+        d
+    }
+
+    #[test]
+    fn one_runnable_exe_exit_zero_returns_ok() {
+        let cmd_path = path_to_test_cmd("run_success.sh");
+        let cmds: [LockCommand; 1] = [LockCommand {
+            exe: cmd_path.to_str().unwrap(),
+            args: &[],
+        }];
+        let result = run_first_found_exe(&cmds);
+        assert_eq!(Ok(0), result);
+    }
+
+    #[test]
+    fn one_runnable_exe_does_not_exist_returns_err() {
+        let cmds: [LockCommand; 1] = [LockCommand {
+            exe: "does_not_exist",
+            args: &[],
+        }];
+        let result = run_first_found_exe(&cmds);
+        assert_eq!(Err(crate::Error::NoExeFound), result);
+    }
+
+    #[test]
+    fn multiple_runnable_exe_none_exist_returns_err() {
+        let cmds: [LockCommand; 3] = [
+            LockCommand {
+                exe: "does_not_exist_1",
+                args: &[],
+            },
+            LockCommand {
+                exe: "does_not_exist_2",
+                args: &[],
+            },
+            LockCommand {
+                exe: "does_not_exist_3",
+                args: &[],
+            },
+        ];
+        let result = run_first_found_exe(&cmds);
+        assert_eq!(Err(crate::Error::NoExeFound), result);
+    }
+
+    #[test]
+    fn multiple_runnable_exe_one_exist_returns_ok_with_index() {
+        let cmd_path = path_to_test_cmd("run_success.sh");
+
+        let cmds: [LockCommand; 3] = [
+            LockCommand {
+                exe: "does_not_exist_1",
+                args: &[],
+            },
+            LockCommand {
+                exe: cmd_path.to_str().unwrap(),
+                args: &[],
+            },
+            LockCommand {
+                exe: "does_not_exist_3",
+                args: &[],
+            },
+        ];
+        let result = run_first_found_exe(&cmds);
+        assert_eq!(Ok(1), result);
+    }
+
+    #[test]
+    fn runnable_exe_exit_non_zero_returns_err_with_exit_code() {
+        let cmd_path = path_to_test_cmd("run_error_27.sh");
+        let cmds: [LockCommand; 1] = [LockCommand {
+            exe: cmd_path.to_str().unwrap(),
+            args: &[],
+        }];
+        let result = run_first_found_exe(&cmds);
+        assert_eq!(
+            Err(Error::NonZeroExit {
+                cmd: Some(cmd_path.to_str().unwrap().to_string()),
+                exit_code: Some(27)
+            }),
+            result
+        );
+    }
+
+    #[test]
+    fn one_runnable_exe_without_execute_returns_err() {
+        let cmd_path = path_to_test_cmd("run_no_exec.sh");
+        let cmds: [LockCommand; 1] = [LockCommand {
+            exe: cmd_path.to_str().unwrap(),
+            args: &[],
+        }];
+        let result = run_first_found_exe(&cmds);
+        assert_eq!(
+            Err(Error::ExeIoError {
+                cmd: Some(cmd_path.to_str().unwrap().to_string()),
+                kind: std::io::ErrorKind::PermissionDenied,
+                msg: "Permission denied (os error 13)".to_string(),
+            }),
+            result
+        );
+    }
+
+    #[test]
+    fn first_runnable_exe_is_used_even_if_error() {
+        let cmd_path1 = path_to_test_cmd("run_error_27.sh");
+        let cmd_path2 = path_to_test_cmd("run_success.sh");
+
+        let cmds: [LockCommand; 2] = [
+            LockCommand {
+                exe: cmd_path1.to_str().unwrap(),
+                args: &[],
+            },
+            LockCommand {
+                exe: cmd_path2.to_str().unwrap(),
+                args: &[],
+            },
+        ];
+        let result = run_first_found_exe(&cmds);
+        assert_eq!(
+            Err(Error::NonZeroExit {
+                cmd: Some(cmd_path1.to_str().unwrap().to_string()),
+                exit_code: Some(27)
+            }),
+            result
+        );
+    }
+
+    // TODO(smacdo): Write integrations tests for `lock_screen_linux`.
+    //  - Write comprehensive integration tests including scenarios:
+    //    - command that exists and returns non-zero with bad utf name.
+    //    - command that exists and terminates via signal.
+    //    - above but with bad utf name.
+}
